a517209f51b08930ae4782e2805bf177b366d4ce
Filip Rychnavsky <filip.r@email.cz>
2020-02-12 21:54:58 +0100

    Debug Variable

--- projects/helloworld/helloworld.cpp
+++ projects/helloworld/helloworld.cpp
@@ -9,8 +9,10 @@ int main()
 
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code!"};
 
+    int i = 0;
     for (const string& word : msg)
     {
+        ++i;
         cout << word << " ";
     }
     cout << endl;
